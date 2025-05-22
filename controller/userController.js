c98e77122fa1d0f611f864aefbc9cd38b6670443
MD Obidur Rahman Bhuiyan <71771396+obidurovi@users.noreply.github.com>
2025-05-23 04:13:24 +0600

    Handle incoming body request

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel');
 
 const createUser = async(req, res) => {
     const email = req.body.email;
-    const findUser = await User.findOne(email);
+    const findUser = await User.findOne({email: email});
 
     if (!findUser) {
         // Create new user
--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT  = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
+const bodyParser = require("body-parser");
 
 dbConnect();
 
-app.use('/', (req, res) => {
-    res.send("Hello from server side");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/api/user", authRouter);
 
