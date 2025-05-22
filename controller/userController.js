51fb1d38ad8933c9c1523b99be268621e1e9ce34
MD Obidur Rahman Bhuiyan <71771396+obidurovi@users.noreply.github.com>
2025-05-23 00:22:14 +0600

    Create User Function created

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,21 @@
 const User = require('../models/userModel');
 
 
-const createUser = (req, res) => {
+const createUser = async(req, res) => {
     const email = req.body.email;
-}
+    const findUser = await User.findOne(email);
+
+    if (!findUser) {
+        // Create new user
+        const newUser = User.create(req.body);
+        res.json(newUser);
+
+    }else{
+        res.json({
+            msg: "User already exist.",
+            success: false,
+        });
+    }
+};
+
+module.exports = {createUser}
