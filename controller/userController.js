8258e7f289ac2214d0b5130f4e28991d1be12231
MD Obidur Rahman Bhuiyan <71771396+obidurovi@users.noreply.github.com>
2025-05-23 14:23:07 +0600

    Add error handling middleware for NotFound and API errors

--- controller/userController.js
+++ controller/userController.js
@@ -1,21 +1,19 @@
 const User = require('../models/userModel');
+const asyncHandler = require("express-async-handler")
 
 
-const createUser = async(req, res) => {
+const createUser = asyncHandler(async(req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({email: email});
 
     if (!findUser) {
         // Create new user
-        const newUser = User.create(req.body);
+        const newUser = await User.create(req.body);
         res.json(newUser);
 
     }else{
-        res.json({
-            msg: "User already exist.",
-            success: false,
-        });
+        throw new Error('User Already Exist');
     }
-};
+});
 
 module.exports = {createUser}
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,18 @@
+
+// Not Found 
+const notFound = (req, res) => {
+    const error = new Error(`Not Found : ${req.originalUrl}`);
+    req.status(404);
+    next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+    const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
+    res.status(statuscode);
+    res.json({
+        message: err?.message,
+        stack: err?.stack,
+    })
+}
+
+module.exports = {errorHandler , notFound}
