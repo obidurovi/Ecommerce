79013673613c1bc851a8bf0ad00be9fac9389ca2
MD Obidur Rahman Bhuiyan <71771396+obidurovi@users.noreply.github.com>
2025-05-28 19:15:58 +0600

    Get Bearer Token from header

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,12 +7,13 @@ const {
   deleteaUser,
   updatedUser,
 } = require("../controller/userController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/all-users", getallUser);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
 router.put("/:id", updatedUser);
 module.exports = router;
