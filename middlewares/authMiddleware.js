88741f8bc0758cd0740390f608c7ce566218197c
MD Obidur Rahman Bhuiyan <71771396+obidurovi@users.noreply.github.com>
2025-05-28 19:17:37 +0600

    Find user by using decoded

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,9 @@ const authMiddleware = asynchandler(async (req, res, next) => {
     try {
       if (token) {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
+        const user = await User.findById(decoded?.id);
+        req.user = user;
+        next();
       }
     } catch (error) {
       throw new Error("Not Authorized token expired, Please login again");
